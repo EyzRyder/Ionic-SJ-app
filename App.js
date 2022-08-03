5fb93cbd4b06e9824e3b48b4904ccfbc002f2bcd
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-08-02 22:42:39 -0300

    created main

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>MILABAG - Segurança Digital</Text>
+      <Text style={styles.subtitle}> Verificacao de segurançao</Text>
+      <Pressable style={styles.btn}>
+        <Text style={styles.btnText}>Explorar</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    textAlign: 'center',
+  fontSize: '2em',
+  color: '#371c00',
+  fontWeight: 700,
+  padding: '5px',
+},
+  subtitle: {
+  justifyContent: 'center',
+  alignContent: 'center',
+  alignText: 'center',
+  fontSize: '1em',
+  fontWeight: 500,
+  color: '#d3145c',
+  paddingLeft: '5px',
+  paddingRight: '5px',
+},
+  btn: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#d3145c',
+  borderRadius: 10,
+  paddingLeft: '50px',
+  paddingRight: '50px',
+  paddingTop: '5px',
+  paddingBottom: '5px',
+  marginTop: '35px',
+},
+  btnText: {
+  fontSize: 16,
+  lineHeight: 21,
+  fontWeight: 'bold',
+  letterSpacing: 0.25,
+  color: 'white',
+},
 });
