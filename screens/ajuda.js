782b3a67996dda2ff90ec0a23a4d75280be049ec
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-09-02 19:45:05 -0300

    added images

--- screens/ajuda.js
+++ screens/ajuda.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Ajuda({ navigation }) {
     return (
@@ -7,6 +7,11 @@ export default function Ajuda({ navigation }) {
             <Text style={styles.title}>
                 Confira o Domínio e a URL e Busque por Selos de Segurança
             </Text>
+            <Image style={styles.image} source={require('../assets/img/Confira o Domínio e a URL e Busque por Selos de Segurança.png')} />
+            <Text style={styles.text}>
+                Um exemplo de um site nao seguro
+                (um clone de face book)</Text>
+            <Image style={styles.imageFB} source={require('../assets/img/faceboock clone (phishing ).png')} />
             <StatusBar style="auto" />
         </View>
     );
@@ -16,6 +21,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        alignItems: 'center',
     },
     title: {
         textAlign: 'center',
@@ -23,5 +29,34 @@ const styles = StyleSheet.create({
         color: '#371c00',
         fontWeight: 700,
         padding: '5px',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: '1em',
+        color: '#371c00',
+        fontWeight: 500,
+        padding: '5px',
+    },
+    image: {
+        height: '16em',
+        width: '100%',
+        padding: '3px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexWrap: 'nowrap',
+        flexDirection: 'column',
+        alignContent: 'center',
+    },
+    imageFB: {
+        height: '35em',
+        width: '100%',
+        padding: '3px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexWrap: 'nowrap',
+        flexDirection: 'column',
+        alignContent: 'center',
     },
 });
