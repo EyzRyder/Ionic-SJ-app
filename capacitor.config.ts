25b62f68c9ecd7d47101e9d23e73691eaca7bb9e
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-01-31 13:29:06 -0300

    swap to ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sj_url_seguranca',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
