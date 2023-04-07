a539cdf0fafa93700fc33db94b463a6e6b22b114
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-04-07 14:10:11 -0300

    added email function

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,12 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.scss';
 import { router } from './Router';
-import Menu from './components/Menu';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </IonApp>
 );
 
--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,30 +1,64 @@
+// ionic react 
 import {
     IonContent,
     IonInput,
     IonItem,
     IonLabel,
     IonPage,
-    IonButton,
-    IonTextarea
+    IonTextarea,
+    useIonToast
 } from '@ionic/react';
-
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
+// react
+import React, { MutableRefObject, useEffect, useRef } from 'react';
+//components
 import Header from '../components/Header/Header';
+//dependencies
+import emailjs from '@emailjs/browser';
+import { useFetcher } from 'react-router-dom';
+//stye
 import './Contato.scss';
 
 const Contato: React.FC = () => {
-    const form = useRef();
+    const [present] = useIonToast();
+// color:'success'|'danger'|'warning'|'primary'|'secondary'|'tertiary'
+    const presentToast = (message: string, position: 'top' | 'middle' | 'bottom', color: 'success' | 'danger' | 'warning' | 'primary' | 'secondary' | 'tertiary') => {
+        present({
+            message,
+            duration: 1500,
+            position: position,
+            animated: true,
+            color
+        });
+    };
 
-    const sendEmail = (e:any) => {
+
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state == "submitting";
+
+    const formRef = useRef() as MutableRefObject<HTMLFormElement>
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset();
+        }
+    }, [isSubmitting]);
+
+
+    const sendEmail = (e: any) => {
         e.preventDefault();
 
-        emailjs.sendForm(import.meta.env.VITE_REACT_APP_SERVICE_ID, import.meta.env.VITE_REACT_APP_TEMPLATE_ID, form.current, import.meta.env.VITE_REACT_APP_EmJS_PUBLIC_KEY)
+        emailjs.sendForm(
+            import.meta.env.VITE_REACT_APP_SERVICE_ID,
+            import.meta.env.VITE_REACT_APP_TEMPLATE_ID,
+            formRef.current,
+            import.meta.env.VITE_REACT_APP_EmJS_PUBLIC_KEY
+        )
             .then((result) => {
                 console.log(result.text);
+                presentToast(`Mensagem enviado!`, "top",'success');
             }, (error) => {
                 console.log(error.text);
+                presentToast(`Teve um problema ao enviar seu email`, "top",'danger');
             });
     };
 
@@ -33,29 +67,47 @@ const Contato: React.FC = () => {
             <Header name={"Contato"} />
 
             <IonContent fullscreen class='contatoPage' scrollEvents={true} >
-                <form ref={form} className='bg-gray-200 rounded-xl relative py-4 px-6 h-[calc(100%-20px)]' onSubmit={sendEmail}>
+                <fetcher.Form method='post' ref={formRef} className='bg-gray-200 rounded-xl relative py-4 px-6 h-[calc(100%-20px)]' onSubmit={sendEmail}>
                     <IonItem>
                         <IonLabel>Nome</IonLabel>
-                        <IonInput placeholder="Digite seu nome" name='from_name'></IonInput>
+                        <IonInput
+                            placeholder="Digite seu nome"
+                            name='from_name'
+                        ></IonInput>
                     </IonItem>
                     <IonItem>
                         <IonLabel>Email</IonLabel>
-                        <IonInput placeholder="Digite seu email" name='from_email'></IonInput>
+                        <IonInput
+                            placeholder="Digite seu email"
+                            name='from_email'
+                        ></IonInput>
                     </IonItem>
                     <IonItem>
                         <IonLabel>Assunto</IonLabel>
-                        <IonInput placeholder="Digite o assunto" name='assunto'></IonInput>
+                        <IonInput
+                            placeholder="Digite o assunto"
+                            name='assunto'
+                        ></IonInput>
                     </IonItem>
                     <IonItem className="">
                         <IonLabel position="stacked">Messagem</IonLabel>
-                        <IonTextarea className="custom-textarea" placeholder="Sua Messagem" autoGrow={true} name='message'></IonTextarea>
+                        <IonTextarea
+                            className="custom-textarea"
+                            placeholder="Sua Messagem"
+                            autoGrow={true}
+                            name='message'
+                        ></IonTextarea>
                     </IonItem>
-                    <input type="submit" value="Enviar" className='bg-primary-default px-3 py-2 w-full mt-5 rounded-lg text-white' />
+                    <input
+                        type="submit"
+                        value="Enviar"
+                        className='bg-primary-default px-3 py-2 w-full mt-5 rounded-lg text-white' />
 
                     {/* <IonButton className="" expand="block">Enviar</IonButton> */}
 
-                </form>
+                </fetcher.Form>
             </IonContent>
+
         </IonPage>
     );
 };
